b9ca5f8c3ade21a5b7077d7665f76dc449e26fdf
J.M. Cléry <jmclery@gmail.com>
2018-04-24 18:34:02 +0200

    Récupération liste collaborateurs du serveur + MAJ de <UserCard>

--- src/components/UserCard.vue
+++ src/components/UserCard.vue
@@ -1,25 +1,30 @@
 <template>
   <article class="user-card">
     <figure>
-      <img src="USER_PHOTO" alt="USER_NAME">
+      <img :src="user.photo" :alt="user.firstname + user.lastname">
     </figure>
     <div>
-      <h1>Prénom NOM</h1>
+      <h1>{{user.firstname}} {{user.lastname}}</h1>
       <p>
-        <i class="fa fa-envelope" /> <a href="mailto:USER_EMAIL" title="Envoyer un email à USER_EMAIL">USER_EMAIL</a><br>
-        <i class="fa fa-phone" /> <a href="tel:USER_PHONE" title="Téléphoner à USER_NAME">USER_PHONE</a><br>
-        <i class="fa fa-calendar" /> USER_BIRTHDATE (XX ans)<br>
-        <i class="fa fa-map-marker" /> USER_CITY, USER_COUNTRY
+        <i class="fa fa-envelope" /> <a :href="'mailto:'+user.email" :title="'Envoyer un email à '+user.firstname +' '+ user.lastname+' ('+user.email+')'">{{user.email}}</a><br>
+        <i class="fa fa-phone" /> <a :href="'tel:'+user.phone" :title="'Téléphoner à '+user.firstname +' '+ user.lastname+' ('+user.phone+')'">{{user.phone}}</a><br>
+        <i class="fa fa-calendar" /> {{user.birthdate }} (XX ans)<br>
+        <i class="fa fa-map-marker" /> {{user.city}}, {{user.country}}
       </p>
-      <a href="#" class="btn btn-small">Supprimer</a>
-      <a href="#" class="btn btn-small">Modifier</a>
+      <a href="#" class="btn btn-small" >Supprimer</a>
+      <a href="#" class="btn btn-small" >Modifier</a>
     </div>
   </article>
 </template>
 
 <script>
 export default {
-  name : 'UserCard'
+  name : 'UserCard',
+
+  // Décrit que ce composant doit impérativement implémenter une prop nomée 'user', et qui doit être un objet
+  props : {
+    user : { type: Object, required: true }
+  }
 }
 </script>
 
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -6,7 +6,7 @@
     <hr>
     <h3>Avez-vous dit bonjour aujourd'hui à ...</h3>
 
-    <UserCard />
+    <UserCard :user="user" v-if="user" />
 
     <div class="actions">
       <a href="#" class="btn">
@@ -18,12 +18,28 @@
 </template>
 
 <script>
+import axios from 'axios'
 import UserCard from '@/components/UserCard.vue'
 
 export default {
   name : 'Home',
 
-  components : { UserCard }
+  components : { UserCard },
+
+  data() {
+    return {
+      users : [], // Contiendra la liste complète des utilisateurs, en provenance du serveur back-end
+      user : null // Contiendra un objet utilisateur choisi au hasard parmi la liste ci-dessus
+    }
+  },
+
+  created() {
+    // A la création de <Home> , on lance une requête Ajax vers le serveur, grâce à la librairie `axios`
+    axios.get('http://localhost:9000/collaborateurs').then( response => {
+      this.users = response.data; // Lorsqu'on obtient la réponse JSON du serveur, on remplit notre tableau `users` défini dans les data
+      this.user = this.users[Math.floor(Math.random() * this.users.length)]; // Prend un utilisateur au hasard dans le tableau `users`
+    })
+  }
 }
 </script>
 
