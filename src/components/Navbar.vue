1cb9a6b373ea65c4292fc81e633bfe621edf8d25
J.M. Cléry <jmclery@gmail.com>
2018-04-24 18:02:42 +0200

    Ajout du composant <Navbar>

---
+++ src/components/Navbar.vue
@@ -0,0 +1,56 @@
+<template>
+  <nav class="navbar">
+    <div class="container">
+      <a href="#" class="active">Accueil</a>
+      <a href="#">Liste des collaborateurs</a>
+      <a href="#">Ajouter un collaborateur</a>
+    </div>
+  </nav>
+</template>
+
+<script>
+export default {
+  name : 'Navbar'
+}
+</script>
+
+<style scoped>
+  .navbar {
+    background-color: #42b983;
+    overflow: hidden;
+    margin-bottom: 3em;
+  }
+  .navbar > .container {
+    display: flex;
+    flex-flow: row wrap;
+    justify-content: center;
+    align-items: center;
+  }
+    .navbar a:link,
+    .navbar a:visited {
+      color: white;
+      text-decoration: none;
+      display: inline-block;
+      padding: 0.75em 1em;
+      transition: all 150ms;
+      transform: scale(1);
+    }
+    .navbar a:hover {
+      background-color: #128953;
+      transform: scale(1.05);
+    }
+    .navbar a.active {
+      background-color: #128953;
+    }
+    .navbar a:active {
+      transition: none;
+      color: #128953;
+      background-color: #82f9c3
+    }
+
+    .navbar a:last-of-type {
+      margin-left: auto;
+    }
+</style>
+
+
